a395f7824cfb5a116ae1d5741ef683987333770a
furkancanturk <furkan.canturk@ozu.edu.tr>
2025-07-31 10:21:11 +0300

    bootstrap_mcp updated and bootstrap changes applied

--- service_core/bootstrap.py
+++ service_core/bootstrap.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append(".")
+sys.path.append("..")
 import threading
 import os
 
@@ -8,34 +9,43 @@ from dotenv import load_dotenv
 import json
 from pathlib import Path
 from service_core.logger import get_logger
-from service_core.config import load_services
+from service_core.config import load_services, load_service_config, get_user_credentials
 
 from service_core.rpc_server import start_rpc_consumer, start_rpc_consumer_mcp
+from mcp.app import main as mcp_main
 
 load_dotenv()
 
-DEPLOYMENT_CODE = os.getenv("DEPLOYMENT_CODE")
-SERVICE_CODE = os.getenv("SERVICE_CODE")
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = Path(__file__).resolve().parent.parent    
 IDEP_DIR = os.path.join(PROJECT_DIR, "configs")
 
-USERNAME = f"{SERVICE_CODE}_0@{DEPLOYMENT_CODE}"
-PASSWORD = f"{SERVICE_CODE}_0@{DEPLOYMENT_CODE}"
-
 logger = get_logger(__name__)
+
+def get_thread(func, **kwargs):
+    return threading.Thread(target=func, kwargs=kwargs, daemon=True)
 
 def bootstrap():
 
-    service = load_services()  # config.py içindeki yeni versiyon tek bir service döndürüyor
-
-    def get_thread(func, **kwargs):
-        t = threading.Thread(target=func, kwargs=kwargs, daemon=True)
-        #t.start()
-        return t
-    # Listen to ConsumedMessages
+    service = load_service_config()
+    username, password = get_user_credentials()
 
     threads = []
+    # for entry in service.get("ProducedMessages", []):
+    #     for q in entry.get("InputQueues", []):
+    #         t = get_thread(
+    #             start_rpc_consumer,
+    #             queue=q["MBQueueName"],
+    #             host=q.get("MBHost", "localhost"),
+    #             port=q.get("MBPort", 5672),
+    #             username=username,
+    #             password=password,
+    #             virtual_host=q.get("MBVirtualHostName", "/"),
+    #             #idep_dir=IDEP_DIR
+    #         )
+    #         threads.append(t)
+
+    # Listen to ConsumedMessages
     for entry in service.get("ConsumedMessages", []):
         for q in entry.get("InputQueues", []):
             t = get_thread(
@@ -43,22 +53,8 @@ def bootstrap():
                 queue=q["MBQueueName"],
                 host=q.get("MBHost", "localhost"),
                 port=q.get("MBPort", 5672),
-                username=USERNAME,
-                password=PASSWORD,
-                virtual_host=q.get("MBVirtualHostName", "/"),
-                idep_dir=IDEP_DIR
-            )
-            threads.append(t)
-    # Listen to RPCMessages_Server
-    for entry in service.get("RPCMessages_Server", []):
-        for q in entry.get("InputQueues", []):
-            t = get_thread(
-                start_rpc_consumer,
-                queue=q["MBQueueName"],
-                host=q.get("MBHost", "localhost"),
-                port=q.get("MBPort", 5672),
-                username=USERNAME,
-                password=PASSWORD,
+                username=username,
+                password=password,
                 virtual_host=q.get("MBVirtualHostName", "/"),
             )
             threads.append(t)
@@ -84,49 +80,49 @@ def get_service_dict(service_code, deployment_code, idep_dir):
         raise ValueError(f"ServiceCode '{service_code}' bulunamadı in {deployment_code}.idep")
     return active_service
 
-def bootstrap_mcp(mcp, all_tools, all_resource_templates):
-  load_dotenv()
+def bootstrap_mcp():
 
-  #service = get_service_dict(service_code=SERVICE_CODE, deployment_code=DEPLOYMENT_CODE, idep_dir="configs")
-  service = load_services()
-  def launch_thread(func, **kwargs):
-      t = threading.Thread(target=func, kwargs=kwargs, daemon=True)
-      t.start()
+    mcp_server, all_tools, all_resource_templates = mcp_main()
+    service = load_service_config()
+    username, password = get_user_credentials()
+    def launch_thread(func, **kwargs):
+        t = threading.Thread(target=func, kwargs=kwargs, daemon=True)
+        t.start()
 
-    # Listen to RPCMessages_Server
-  for entry in service.get("RPCMessages_Server", []):
-      for q in entry.get("InputQueues", []):
-          launch_thread(
-              start_rpc_consumer_mcp,
-              all_tools=all_tools,
-              all_resource_templates=all_resource_templates,
-              mcp=mcp,
-              queue=q["MBQueueName"],
-              host=q.get("MBHost", "localhost"),
-              #host="rabbitmq",
-              port=q.get("MBPort", 5672),
-              username=USERNAME,
-              password=PASSWORD,
-              virtual_host=q.get("MBVirtualHostName", "/"),
-          )
+        # Listen to RPCMessages_Server
+    for entry in service.get("RPCMessages_Server", []):
+        for q in entry.get("InputQueues", []):
+            launch_thread(
+                start_rpc_consumer_mcp,
+                all_tools=all_tools,
+                all_resource_templates=all_resource_templates,
+                mcp=mcp_server,
+                queue=q["MBQueueName"],
+                host=q.get("MBHost", "localhost"),
+                port=q.get("MBPort", 5672),
+                username=username,
+                password=password,
+                virtual_host=q.get("MBVirtualHostName", "/"),
+            )
 
-  print("✅ All queues are now being listened to.")
+    print("✅ All queues are now being listened to.")
 
-  mcp.run(transport="http", port=8000, log_level="debug", host="0.0.0.0")
- 
+    mcp_server.run(transport="http", port=8000, log_level="debug", host="0.0.0.0")
+
 if __name__ == "__main__":
-  
-  parser = argparse.ArgumentParser(description="Run a specific task.")
-  parser.add_argument("--task", required=True, help="Name of the task to run")
 
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Run a specific task.")
+    parser.add_argument("--task", default="llm", help="Name of the task to run")
 
-  # You can add conditional logic here
-  if args.task == "llm":
-    bootstrap()
-  elif args.task == "mcp":
-    from my_mcp import mcp, transformed_tools, transformed_resource_templates
-    bootstrap_mcp(mcp, transformed_tools, transformed_resource_templates)
-  else:
-    print(f"Unknown task: {args.task}")
-    sys.exit(1)
+    args = parser.parse_args()
+
+    # You can add conditional logic here
+    if args.task == "llm":
+        bootstrap()
+    elif args.task == "mcp":
+        bootstrap_mcp()
+    else:
+        print(f"Unknown task: {args.task}")
+        sys.exit(1)
+
+
